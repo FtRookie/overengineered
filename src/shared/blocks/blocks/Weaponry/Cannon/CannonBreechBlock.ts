e7a53be1ec42a2da57cf2336113ce41fd3cf6f04
FtRookie <email@ftrookie.com>
2026-08-02 11:31:59 -0700

    1 weapon sound effect call site

--- src/shared/blocks/blocks/Weaponry/Cannon/CannonBreechBlock.ts
+++ src/shared/blocks/blocks/Weaponry/Cannon/CannonBreechBlock.ts
@@ -169,5 +169,5 @@ export const CannonBreech = {
 		},
 	},
 
-	logic: { definition, ctor: Logic, events: { fire: WeaponFireSound.event } },
+	logic: { definition, ctor: Logic, events: WeaponFireSound.events },
 } as const satisfies BlockBuilder;
